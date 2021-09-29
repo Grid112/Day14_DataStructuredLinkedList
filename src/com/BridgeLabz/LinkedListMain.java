db9e5277ce04b9285354d7ca481d50bcb7aecfae
Grid112 <sumitware1999@gmail.com>
2021-09-29 20:50:58 +0530

    Appending Data To LinkedList

--- src/com/BridgeLabz/LinkedListMain.java
+++ src/com/BridgeLabz/LinkedListMain.java
@@ -1,9 +1,47 @@
 package com.BridgeLabz;
 
+import java.util.Scanner;
+
 public class LinkedListMain {
+    Node head;
+
+    //Add Node Method Is Created To Add Nodes To LinkedList
+    public void addNode(int data){
+        Node newNode = new Node(data);
+        newNode.next = null;
+        if(this.head == null){
+            head = newNode;
+        }
+        else{
+            Node last = this.head;
+            while(last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+    }
+
+    //display Method Is created To Display linkedList
+    public void display(){
+        if(head == null){
+            System.out.println("linked list has no value");
+        }
+        Node node = head;
+        while(node.next != null){
+            System.out.print("->"+node.data);
+            node = node.next;
+        }
+        if(node.next == null){
+            System.out.print("->"+node.data);
+        }
+    }
     public static void main(String args[]) {
         System.out.println("welcome to linked list");
-        //create obejct and give to the method
         LinkedListMain linkedList = new LinkedListMain();
+        linkedList.addNode(70);
+        linkedList.addNode(30);
+        linkedList.addNode(56);
+        linkedList.display();
+
     }
 }
