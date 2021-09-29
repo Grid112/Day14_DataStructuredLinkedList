898f2be8208a0c54c901f3729689569ba022bb60
Grid112 <sumitware1999@gmail.com>
2021-09-29 20:34:25 +0530

    Simple Linked List

---
+++ src/com/BridgeLabz/LinkedListMain.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz;
+
+public class LinkedListMain {
+    public static void main(String args[]) {
+        System.out.println("welcome to linked list");
+        //create obejct and give to the method
+        LinkedListMain linkedList = new LinkedListMain();
+    }
+}
---
+++ src/com/BridgeLabz/Node.java
@@ -0,0 +1,11 @@
+package com.BridgeLabz;
+
+public class Node {
+    int data;
+    Node next;
+    Node(int d){
+        data = d;
+        next = null;
+    }
+}
+
